b5531ccdfc04d60afd4faa4b14c5a6aa359d60e1
sci <kacper17@outlook.com>
2020-10-12 14:10:35 +0200

    Working on OTFCC import
    Former-commit-id: 9d7aa685390daf801c4345264df3002e0b300fe2 [formerly 88227f52bc2f5fb9d3178edadf9081b5eaf9e8f0]
    Former-commit-id: b890bf51cfce88d7915bcd5e0dff7800a992b8d7

--- src/typeedit/io/otfcc/import.ts
+++ src/typeedit/io/otfcc/import.ts
@@ -1,7 +1,9 @@
 import { exec } from "child_process"
 import { join } from "path"
 import { Font } from "../../font/font"
-import { OTFCCData } from "./types"
+import { Glyph } from "../../font/glyph"
+import { undo } from "../../undo/history"
+import { NumberOrVariation, OTFCCData } from "./types"
 
 let otfccPath: string
 
@@ -9,11 +11,53 @@ export function setOtfccPath(path: string) {
     otfccPath = path
 }
 
-function otfccToFont(data: OTFCCData) {
-    // return new Font({}, {}, [])
+const novar = (x: NumberOrVariation) => {
+    if (typeof x === "number") return x
+    return x[0]
 }
 
-export function importFont_otfcc(filePath: string) {
+function otfccToGlyphs(font: Font, data: OTFCCData): Glyph[] {
+    return data.glyph_order.map(
+        name => {
+            const glyf = data.glyf[name]
+            const unicode = +Object.keys(data.cmap)[
+                Object.values(data.cmap).indexOf(name)
+            ]
+
+            const scaleFactor = 512 / data.OS_2.sCapHeight
+
+            return new Glyph(
+                font,
+                name,
+                isNaN(unicode) ? 0 : unicode,
+                {
+                    leftBearing: 0,
+                    rightBearing: (
+                        novar(glyf.advanceWidth)
+                    ) * scaleFactor
+                },
+                []
+            )
+        }
+    )
+}
+
+function otfccToFont(data: OTFCCData) {
+    if (data.OS_2.version < 4) return
+    const scaleFactor = 512 / data.OS_2.sCapHeight
+    
+    return new Font(
+        {},
+        {
+            descender: -data.hhea.descender * scaleFactor + 512,
+            ascender: -(data.hhea.ascender - data.OS_2.sCapHeight) * scaleFactor,
+            xHeight: (data.OS_2.sCapHeight - data.OS_2.sxHeight) * scaleFactor
+        },
+        []
+    )
+}
+
+export function importFont_otfcc(filePath: string): Promise<Font> {
     if (!otfccPath) return
 
     return new Promise(
@@ -27,8 +71,14 @@ export function importFont_otfcc(filePath: string) {
                     const data = JSON.parse(stdout)
                     const font = otfccToFont(data)
 
+                    font.glyphs = otfccToGlyphs(
+                        font, data
+                    )
+
                     console.log(data)
                     console.log(font)
+
+                    resolve(font)
                 }
             )
         }
--- src/typeedit/io/otfcc/types.ts
+++ src/typeedit/io/otfcc/types.ts
@@ -36,7 +36,7 @@ type VariationItem = {
     delta: number
     on: string
 }
-type NumberOrVariation = number | [number, ...VariationItem[]]
+export type NumberOrVariation = number | [number, ...VariationItem[]]
 
 type GLYFPoint = {
     on: boolean
@@ -116,10 +116,64 @@ type COLRTable = {
     }[]
 }[]
 
+// OS/2
+type OS2v0Body = {
+    xAvgCharWidth: number
+    usWeightClass: number
+    usWidthClass: number
+    fsType: {} // TODO
+    ySubscriptXSize: number
+    ySubscriptYSize: number
+    ySubscriptXOffset: number
+    ySubscriptYOffset: number
+    ySuperscriptXSize: number
+    ySuperscriptYSize: number
+    ySuperscriptXOffset: number
+    ySuperscriptYOffset: number
+    yStrikeoutSize: number
+    yStrikeoutPosition: number
+    sFamilyClass: number
+    panose: number[]
+    ulUnicodeRange1: {} // TODO
+    ulUnicodeRange2: {}
+    ulUnicodeRange3: {}
+    ulUnicodeRange4: {}
+    achVendID: string
+    fsSelection: {} // TODO
+    usFirstCharIndex: number
+    usLastCharIndex: number
+    sTypoAscender: number
+    sTypoDescender: number
+    sTypoLineGap: number
+    usWinAscent: number
+    usWinDescent: number
+}
+
+type OS2v1Body = OS2v0Body & {
+    ulCodePageRange1: {} // TODO
+    ulCodePageRange2: {}
+}
+
+type OS2v4Body = OS2v1Body & {
+    sxHeight: number
+    sCapHeight: number
+    usDefaultChar: number
+    usBreakChar: number
+    usMaxContext: number
+}
+
+type OS2v5Body = OS2v4Body & {
+    usLowerOpticalPointSize: number
+    usUpperOpticalPointSize: number
+}
+
+export type OS2Table =  { version: 4 } & OS2v4Body
+
 export type OTFCCData = {
     hhea: HHEATable
     head: HEADTable
     name: NAMETable
+    OS_2: OS2Table
 
     glyph_order: string[]
     glyf: GLYFTable
